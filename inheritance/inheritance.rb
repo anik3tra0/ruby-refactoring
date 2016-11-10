1763a2fcd4887264ad64aa9a0a297bce132bae14
anik3tra0 <anik3tra0@gmail.com>
2016-11-10 23:34:17 +0530

    Inheritance Completed

--- inheritance/inheritance.rb
+++ inheritance/inheritance.rb
@@ -1,1 +1,37 @@
-"Hello World!"
+# Let's begin by exploring the principles of Object Oriented Design
+
+class Person
+	
+	attr_reader :first_name, :last_name
+
+	def initialize first_name, last_name, gender
+		@first_name = first_name
+		@last_name = last_name
+		@gender = gender
+	end
+
+	def full_name
+		first_name + " " + last_name
+	end
+
+end
+
+# Screencaster Inherits properties from Person
+class Screencaster < Person
+
+	def initialize first_name, last_name, gender, tools
+		super first_name, last_name, gender
+		@tools = tools
+	end
+
+end
+
+# Student Inherits properties from Person
+class Student < Person
+
+	def initialize first_name, last_name, gender, preferred_language
+		super first_name, last_name, gender
+		@preferred_language = preferred_language
+	end
+
+end
