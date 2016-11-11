9451b3b7b6bdbb32824d5d8be4cf73daea679f28
anik3tra0 <anik3tra0@gmail.com>
2016-11-11 07:51:55 +0530

    Polymorphism Completed

---
+++ polymorphism/polymorphism.rb
@@ -0,0 +1,51 @@
+# Polymorphism is a way of implementing variations of a base class method!
+
+class Person
+	attr_reader :first_name, :last_name, :gender
+
+	def initialize first_name, last_name, gender
+		@first_name = first_name
+		@last_name = last_name
+		@gender = gender
+	end
+
+	def full_name
+		first_name + " " + last_name
+	end
+
+	def present
+		raise NotImplementedError, "Must be implemented by subtypes"
+		# %Q{Hello, my name is #{full_name} and I am a #{gender}}
+
+		# By commenting and raising an custom exception. we are emphasizing that the present method can be called on an instance of any of Person Classes' Subclasses'
+
+		# E.g. Screencaster.first.present will give you a desired output
+	end
+
+end
+
+class Screencaster < Person
+
+	def initialize first_name, last_name, gender, tools
+		super first_name, last_name, gender
+		@tools = tools
+	end
+
+	def present
+		%Q{Hello, welcome to DCT Learn Ups! My name is #{full_name} and I'll be your guide!}
+	end
+
+end
+
+class Student < Person
+
+	def initialize first_name, last_name, gender, preferred_language
+		super first_name, last_name, gender
+		@preferred_language = preferred_language
+	end
+
+	def present
+		%Q{Hows' it going guys! I am #{full_name} (#{gender}). Glad to be part of this awesome learn up! :)}
+	end
+
+end
