cb5e5fa5265f9965bbf3809aa6d92a8c77b2c51a
anik3tra0 <anik3tra0@gmail.com>
2016-11-11 08:20:54 +0530

    The 4 Principles Completed

--- README.md
+++ README.md
@@ -5,8 +5,11 @@
 ### What is *Refactoring* ?
 *Code refactoring is the process of restructuring existing computer code—changing the factoring—without changing its external behaviour. Refactoring improves nonfunctional attributes of the software. Advantages include improved code readability and reduced complexity; these can improve source-code maintainability and create a more expressive internal architecture or object model to improve extensibility.*
 
-### Contents
+### 4 Main Principles of Object Oriented Design
 
 1. Inheritance
 2. Encapsulation
 3. Polymorphism
+4. Duck Typing
+
+### Code Smells
---
+++ ood-principles/ducktyping/duck_typing.rb
@@ -0,0 +1,46 @@
+# Duck Typing allows replying on a message rather than inheritance. In this case its the 'talk' method.
+
+# It's a really famous principle in Ruby because the language allows that in other case where Java or C++ don't. Java and C++ as asthetic languages rely heavily on strong fixed typing. Ruby doesn't have that. It has deep strong typing. It has classes and instances which you can use in many different ways.
+
+# Using Duck Typing you can specify a specific interface for each class. In this case it is the 'talk' method.
+
+# Asthetic Type languages use interfaces and then each method implements that interface. But Ruby on the other hand uses Duck Typing because there are no asthetic typing definations in the language.
+
+class Person
+	attr_reader :first_name, :last_name, :gender
+
+	def initialize first_name, last_name, gender
+		@first_name = first_name
+		@last_name = last_name
+		@gender = gender
+	end
+
+	def talk
+		"Hello World!"
+	end
+
+end
+
+class Animal
+
+	def initialize name
+		@name = name
+	end
+
+	def talk
+		"Bark!, Meow!, Woof!, Moo!"
+	end
+
+end
+
+class Bugs
+
+	def initialize type
+		@type = type
+	end
+
+	def talk
+		"Can bugs talk ?!?"
+	end
+
+end
